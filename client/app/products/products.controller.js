881317d7120287932e9a1ee677be109fbda38c02
David Chang <david.j.chang@gmail.com>
2015-01-13 11:48:33 -0500

    12-DummyDataWorkingBeta

--- client/app/products/products.controller.js
+++ client/app/products/products.controller.js
@@ -5,15 +5,17 @@ angular.module('stackStoreApp')
     $scope.message = 'Hello';
 
     $scope.products = [
-    {name: 'Time Machine', price: 100000},
-    {name: 'Watch', price: 10},
-    {name: 'Clock', price: 5},
-    {name: 'Alarm Clock', price: 3},
-    {name: 'Black Hole', price: 50},
-    {name: 'Paradox', price: 46},
-    {name: 'Time Itself', price: 78},
-    {name: 'Wormhole', price: 2},
-    {name: 'Tardis', price: 50000},
-    {name: 'Time Warp', price:19}
+    {name: 'Time Machine', price: 100000, description: {blurb: "A time machine", full:"More than the last"} },
+    {name: 'Watch', price: 10, description: {blurb: "A time machine", full:"More than the last"} },
+    {name: 'Clock', price: 5, description: {blurb: "A time machine", full:"More than the last"} },
+    {name: 'Alarm Clock', price: 3, description: {blurb: "A time machine", full:"More than the last"} },
+    {name: 'Black Hole', price: 50, description: {blurb: "A time machine", full:"More than the last"} },
+    {name: 'Paradox', price: 46, description: {blurb: "A time machine", full:"More than the last"} },
+    {name: 'Time Itself', price: 78, description: {blurb: "A time machine", full:"More than the last"} },
+    {name: 'Wormhole', price: 2, description: {blurb: "A time machine", full:"More than the last"} },
+    {name: 'Tardis', price: 50000, description: {blurb: "A time machine", full:"More than the last"} },
+    {name: 'Time Warp', price:19, description: {blurb: "A time machine", full:"More than the last"} },
+    {name: 'Time Warp', price:19, description: {blurb: "A time machine", full:"More than the last"} },
+    {name: 'Time Warp', price:19, description: {blurb: "A time machine", full:"More than the last"} }
     ];
   });
