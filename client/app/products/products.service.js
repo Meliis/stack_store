f2492d31ac311b5ba3aaaa46cd9af89fed62aac6
Melissa Moy and Katie Peters <all+melissaomoy+katiejoypeters@gmail.com>
2015-01-13 17:06:44 -0500

    search and get working, added .run to app.js, minor changes to main controller/view for testing

--- client/app/products/products.service.js
+++ client/app/products/products.service.js
@@ -1,16 +1,58 @@
 'use strict';
 
 angular.module('stackStoreApp')
-  .factory('products', function () {
-    // Service logic
-    // ...
-
-    var meaningOfLife = 42;
+  .factory('productFactory', function ($http) {
+    var products = [];
 
     // Public API here
     return {
-      someMethod: function () {
-        return meaningOfLife;
+      setInitial: function (data) {
+        products = data;
+      },
+      getAll: function() {
+        return products;
+      },
+      search: function(str) {
+        var searchResults = [];
+        // overlook articles
+        var cleanedUpSearch = function(query) {
+          var dirtySearch = query.split(" ");
+          dirtySearch.forEach(function(term, i) {
+            if (term.length < 2 || term === "the" || term === "time") {
+              dirtySearch.splice(i, 1);
+            }
+          });
+          return dirtySearch;
+        }
+        var searchTerms = cleanedUpSearch(str);
+        products.forEach(function(product) {
+          var counter = 0;
+          searchTerms.forEach(function(term) {
+            var re = new RegExp(term, "gi");
+            if (re.test(product.name) || re.test(product.description.full)) {
+              counter++;
+            } 
+          });
+          if (counter > 0) {
+            searchResults.push({product: product, terms: counter});
+          }
+        });
+
+        searchResults.sort(function(a,b) {
+          if (a.terms > b.terms) return -1;
+          if (a.terms < b.terms) return 1;
+          return 0;
+        });
+
+        return searchResults; // not sure if needs to be reversed or
+      },
+      browseByCategory: function(category) {
+        // var results = [];
+        // http get category items by name (population happens on back end)
+        // results = those results
+        // return
       }
+      // get single item
+      // add to cart
     };
-  });
+  })
