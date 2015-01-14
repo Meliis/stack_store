ba665ca769b5a476ea94afd107e4ba1767056c37
Chuck Pierce and Katie Peters <all+chuckmpierce+katiejoypeters@gmail.com>
2015-01-14 16:48:18 -0500

    edit seed file to put empty order object in test user

--- client/app/products/productView/productView.controller.js
+++ client/app/products/productView/productView.controller.js
@@ -1,12 +1,35 @@
 'use strict';
 
 angular.module('stackStoreApp')
-  .controller('ProductViewCtrl', function ($scope, productFactory, $routeParams) {
+  .controller('ProductViewCtrl', function ($scope, productFactory, $routeParams, Auth) {
+
+  	$scope.user = Auth.getCurrentUser();
+  	$scope.quantity = 1;
   	
     productFactory.viewProduct($routeParams.id).then(function(product) {
     	$scope.product = product;
     });
 
     $scope.maxStars = [1,2,3,4,5];
+
+    console.log($scope.user);
+
+    $scope.addToCart = function(quantity) {
+
+    	var productExists = false;
+
+    	angular.forEach($scope.user.orders[0].products, function(product) {
+    		if($scope.product._id === product.product._id) {
+    			product.qty += quantity;
+    			productExists = true;
+    		}
+    	});
+
+    	if(productExists === false) {
+	    	$scope.user.orders[0].products.push({product: $scope.product, qty: quantity});    		
+    	}
+
+    	$scope.quantity = 1;
+    };
 
   });
