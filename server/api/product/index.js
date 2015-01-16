a420b7b38d3ecca9c4b208e163f62ec1fe886f0f
Chuck Pierce <chuckmpierce@gmail.com>
2015-01-16 13:05:46 -0500

    add auth admin to other product routes

--- server/api/product/index.js
+++ server/api/product/index.js
@@ -9,8 +9,8 @@ var router = express.Router();
 router.get('/', controller.index);
 router.get('/:id', controller.show);
 router.post('/', auth.hasRole('admin'), controller.create);
-router.put('/:id', controller.update);
+router.put('/:id', auth.hasRole('admin'), controller.update);
 router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.delete('/:id', auth.hasRole('admin'), controller.destroy);
 
 module.exports = router;
