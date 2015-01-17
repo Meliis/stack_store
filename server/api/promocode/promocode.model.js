945b28c694d56ecab35cae648482dae30ff6968c
Chuck Pierce <chuckmpierce@gmail.com>
2015-01-17 16:49:28 -0500

    created promocode schema, statics, and tests

--- server/api/promocode/promocode.model.js
+++ server/api/promocode/promocode.model.js
@@ -1,12 +1,33 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+    Schema = mongoose.Schema,
+    Product = require('../product/product.model');
+
+var types = "all single category".split(' ');
 
 var PromocodeSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  code:{type: String, required: true},
+  creationDate: {type: Date, required: true},
+  expirationDate: {type: Date, required: true},
+  promoType: {type: String, enum: types, required: true},
+  productId: {type: mongoose.Schema.Types.ObjectId, ref: 'Product'}
 });
+
+// Not sure if I need this since the type will always be set on the front end
+// PromocodeSchema.methods.typeCheck = function(type) {
+// 	if(type === 'single') {
+// 		this.promoType = 'single';
+// 	} else if (type === 'category') {
+// 		this.promoType = 'category';
+// 	} else {
+// 		this.promoType = 'all';
+// 	}
+// };
+
+PromocodeSchema.statics.createDate = function(promocode) {
+	promocode.creationDate = new Date();
+	return promocode;
+};
 
 module.exports = mongoose.model('Promocode', PromocodeSchema);
