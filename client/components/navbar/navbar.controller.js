61b8c88918c41533c3662b27321da20e13692bf8
Melissa Moy <melissaomoy@gmail.com>
2015-01-18 19:59:42 -0500

    browse by category visuals in navbar

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,16 +1,13 @@
 'use strict';
 
 angular.module('stackStoreApp')
-  .controller('NavbarCtrl', function ($scope, $location, Auth, Order) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
-
+  .controller('NavbarCtrl', function ($scope, $location, Auth, Order, Category) {
     $scope.isCollapsed = true;
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.isAdmin = Auth.isAdmin;
     $scope.getCurrentUser = Auth.getCurrentUser;
+
+    $scope.categories = Category.query()
 
     $scope.logout = function() {
       Auth.logout();
