5e58154642ed2b427c3c30ea75a3148f3cf30f32
Chuck Pierce and Melissa Moy <all+chuckmpierce+melissaomoy@gmail.com>
2015-01-20 23:40:07 -0500

    works as far as i can tell.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,9 +1,25 @@
 'use strict';
 
 angular.module('stackStoreApp')
-  .controller('MainCtrl', function ($scope, $http, Product, Category) {
+  .controller('MainCtrl', function ($scope, $http, Product, Category, Auth, Cart, User) {
     $scope.products = Product.query();
-
     $scope.categories = Category.query();
 
+    $scope.user = Auth.getCurrentUser();
+    if ($scope.user.$promise) {
+    	$scope.user.$promise.then(function(user) {
+    		if (user.provider == 'google') {
+    			if (user.__v == 0) {
+    				console.log("hello")
+    				user.__v++;
+    				Cart.startAuthCart(user._id);
+    				User.update(user);
+    				Cart.mergeCarts();
+    			}
+    			else {
+    				Cart.mergeCarts(user._id);
+    			}
+    		}
+    	});
+    }
   });
